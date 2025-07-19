e40c8b00716da6e685341e13c08b23e1833ced6a
Jason Liu <jliu6162@gmail.com>
2025-07-19 02:53:57 -0400

    serial testing

--- arduino_serial.py
+++ arduino_serial.py
@@ -1,1 +1,20 @@
-#test commit
+import serial
+import time
+
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+time.sleep(2)
+
+azimuth=35
+elevation=10
+fire=1
+
+message = "A:"+str(azimuth)+";E:"+str(elevation)+";F:"+str(fire)+"\n"
+
+serial.write(message)
+
+while True:
+    if ser.in_waiting:
+        response = ser.readline().decode().strip()
+        print("received", response)
+        break
