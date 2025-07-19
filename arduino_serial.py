78be2a31ca6c8b7237d0cc6fcc96e45c59ab518d
Jason Liu <jliu6162@gmail.com>
2025-07-19 04:43:05 -0400

    pipe

--- arduino_serial.py
+++ arduino_serial.py
@@ -2,6 +2,8 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+cam = serial.Serial('/dev/ttyACM0', 11600,timeout=1)
+
 time.sleep(2)  # Give time for Arduino to reset
 
 azimuth = "35"
@@ -16,6 +18,11 @@ ser.write(message.encode())
 
 # Optionally, wait for a response from Arduino
 while True:
+    if cam.in_waiting:
+        data = cam.readline().decode().strip()
+        
+
+
     if ser.in_waiting:
         response = ser.readline().decode().strip()
         print("received", response)
--- camera_listener.py
+++ camera_listener.py
@@ -6,3 +6,6 @@ time.sleep(2)
 while True:
     if ser.in_waiting > 0:
         print(ser.readline().decode().strip())
+    else:
+        print("Waiting...")
+        time.sleep(0.15)
